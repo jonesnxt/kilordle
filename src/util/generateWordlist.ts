ccbb49c05ea4ef7caf86c2a6469f3bd16e11ee76
toBeOfUse <mitchjacov@gmail.com>
2022-03-28 18:10:06 -0400

    make word list length configurable for testing

--- src/util/generateWordlist.ts
+++ src/util/generateWordlist.ts
@@ -1,13 +1,13 @@
 import { seedRandom } from './seedRandom';
 import { WORDLES, NUM_WORDLES } from './words'
 
-export function generateWordlist() {
+export function generateWordlist(length: number = 1000) {
     const seed = seedRandom();
     const generator = 163; // n is a cyclic group so I can put any prime as a generator (maybe?)
 
     const wordlist = [];
 
-    for(let i = 0; i < 1000; i++) {
+    for(let i = 0; i < length; i++) {
         wordlist.push(WORDLES[(seed + (generator*i)) % NUM_WORDLES])
     }
 
