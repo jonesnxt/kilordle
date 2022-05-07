7275b3d0f640c6cf95e5590a74b7e069209f1fff
briaguya <briaguya>
2022-05-07 01:43:42 -0400

    multiple words are getting a score of 15

--- src/util/sortByValue.ts
+++ src/util/sortByValue.ts
@@ -2,11 +2,24 @@ export function sortByValue(words: string[], guesses: string[]) {
   // iterate through each word and give it a score
   let scores = words.map((word) => scoreWordle(word, guesses));
 
-  return scores
-    .map<[string, number]>((score, i) => [words[i], score])
-    .filter((p) => p[1] !== 15)
-    .sort((a, b) => b[1] - a[1])
-    .map((k) => k[0])
+  let mappedScores = scores.map<[string, number]>((score, i) => [words[i], score])
+
+  let filteredScores = mappedScores.filter((p) => p[1] !== 15)
+
+  let filteredBlargScores = mappedScores.filter((p) => p[1] === 15)
+  console.log(filteredBlargScores)
+
+  let sortedScores = filteredScores.sort((a, b) => b[1] - a[1])
+
+  let mappedAgainScores = sortedScores.map((k) => k[0])
+
+  return mappedAgainScores
+
+  // return scores
+  //   .map<[string, number]>((score, i) => [words[i], score])
+  //   .filter((p) => p[1] !== 15)
+  //   .sort((a, b) => b[1] - a[1])
+  //   .map((k) => k[0])
 }
 
 export function scoreWordle(word: string, guesses: string[]) {
